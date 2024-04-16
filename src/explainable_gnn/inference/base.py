afe627d12a8639641fccf7e2747853edbbb144af
themaigod <2271108464@qq.com>
2024-04-16 17:09:22 +0800

    corrections for inference
    Signed-off-by: themaigod <2271108464@qq.com>

--- src/explainable_gnn/inference/base.py
+++ src/explainable_gnn/inference/base.py
@@ -1,5 +1,5 @@
 import explainable_gnn as eg
-from .general import general_inference_method
+from .general import _general_inference_method
 
 
 class InferenceModel(eg.Model):
@@ -9,7 +9,7 @@ class InferenceModel(eg.Model):
         if getattr(model, "inference_method", None) is not None:
             model.inference_method(self, **kwargs)
         else:
-            general_inference_method(self, **kwargs)
+            _general_inference_method(self, **kwargs)
 
     def inference(self, *args, **kwargs):
         if not self.inference_method:
--- src/explainable_gnn/inference/general.py
+++ src/explainable_gnn/inference/general.py
@@ -1,8 +1,8 @@
 from .base import InferenceModel
 
 
-def general_inference_method(inference_model: InferenceModel, **kwargs):
+def _general_inference_method(inference_model: InferenceModel, **kwargs):
     # TODO: Implement the general inference method in the future
     # should set a inference_method and deploy_method for the inference_model
-    # deploy_method can call general_deploy_method
+    # deploy_method can call _general_deploy_method from deploy/general.py
     pass
